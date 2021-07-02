eaaffdb024edbbd9d65abbe4b8fbc488ce54703a
Sebastian Husche <sebastian@scanbot.io>
2021-07-02 18:12:59 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .binaryTarget(
             name: "ScanbotSDK",
             url: "https://download.scanbot.io/sdk/ios/xcframeworks/scanbot-ios-sdk-xcframework-1.19.1.zip",
-            checksum: "b0d9a5c5124a64b355f2a18fbd56d45e531088fc8fd10eb2d2d99def30e8e324"
+            checksum: "a2d12dca2c5fad4a2cff4114ba1b7c841489310b4481f641f06fefdd5677cdd1"
         ),
         .target(name: "AdditionalData",
                 dependencies: ["ScanbotSDK"],
