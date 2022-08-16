f07046cd104ce82d58fcdb7f926ee12b7f52e382
scanbot-ci <ci@scanbot.io>
2022-08-16 10:07:39 +0200

    Publishing SPM package for release v1.28.1-RC2.

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ScanbotSDK",
-            url: "https://download.scanbot.io/sdk/ios/pre/xcframeworks/RC1/scanbot-ios-sdk-xcframework-1.28.1.zip",
-            checksum: "ccd5eb4acfeda84e02a09f297ea31d3b4d827bb862f771f69622131f09fae93a"
+            url: "https://download.scanbot.io/sdk/ios/pre/xcframeworks/RC2/scanbot-ios-sdk-xcframework-1.28.1.zip",
+            checksum: "79d7b75dd85cf581376296743d3de29af5fe5c2d01e1cd23432b123b01d2fe78"
         ),
         .target(name: "AdditionalData",
                 dependencies: ["ScanbotSDK"],
