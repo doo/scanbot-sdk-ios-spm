6e59ad091d81bdcbff7fcb1798c62efb686ff53a
scanbot-ci <ci@scanbot.io>
2022-11-08 11:06:44 +0100

    Publishing SPM package for release v1.30.2-Beta4.

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ScanbotSDK",
-            url: "https://download.scanbot.io/sdk/ios/pre/xcframeworks/Beta3/scanbot-ios-sdk-xcframework-1.30.2.zip",
-            checksum: "923e3e45e36fe4c645904a5c7a63f193122702f6faaf79169675e418b44ca848"
+            url: "https://download.scanbot.io/sdk/ios/pre/xcframeworks/Beta4/scanbot-ios-sdk-xcframework-1.30.2.zip",
+            checksum: "04c810a4c29ab8c47256d71fd96a4c86d2cc85611ef31ab26035779b5637ce38"
         ),
         .target(name: "AdditionalData",
                 dependencies: ["ScanbotSDK"],
